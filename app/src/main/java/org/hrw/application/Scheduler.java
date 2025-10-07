66c3f9baf91077923c81caa9b79c0702122a6f59
MatWo1996 <matthias.wohlmacher@realcore.de>
2025-10-07 11:27:57 +0200

    Scheduler added

--- app/src/main/java/org/hrw/application/Scheduler.java
+++ app/src/main/java/org/hrw/application/Scheduler.java
@@ -1,4 +1,45 @@
 package org.hrw.application;
 
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Scheduler {
+    Timer timer;
+    TimerTask timerTask;
+    int timePeriod;
+
+    public Scheduler(SchedulerBuilder builder) {
+        timer = new Timer();
+        this.timerTask = builder.timerTask;
+        this.timePeriod = builder.timePeriod;
+    }
+
+    public void executeTimer(Date firstExecution) {
+        timer.schedule(timerTask, firstExecution, timePeriod * 1000L);
+    }
+
+    public void stop(){
+        timer.cancel();
+    }
+
+    public static class SchedulerBuilder {
+        TimerTask timerTask;
+        int timePeriod;
+
+        public SchedulerBuilder setTimerTask(TimerTask timerTask) {
+            this.timerTask = timerTask;
+            return this;
+        }
+
+        public SchedulerBuilder setTimePeriod(int timePeriod) {
+            this.timePeriod = timePeriod;
+            return this;
+        }
+
+        public Scheduler build() {
+            return new Scheduler(this);
+        }
+    }
 }
+
